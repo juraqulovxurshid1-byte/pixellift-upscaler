eda6849cb409215df2a3ce109b141ef2644b2910
juraqulovxurshid1-byte <juraqulovxurshid1@gmail.com>
2026-06-30 12:57:03 +0500

    Update sw.js for www domain

--- public/sw (1).js
+++ public/sw (1).js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11219431
+    "zoneId": 11219497
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
