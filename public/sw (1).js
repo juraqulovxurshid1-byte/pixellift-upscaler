f45164aabf92cfbfff52b5e23f9eca00f0145a17
juraqulovxurshid1-byte <juraqulovxurshid1@gmail.com>
2026-06-30 12:50:27 +0500

    Update Monetag token for custom domain

--- public/sw (1).js
+++ public/sw (1).js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11219232
+    "zoneId": 11219431
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
